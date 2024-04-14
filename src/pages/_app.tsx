454d911fa37e2e398661cba241f6adde54ba1136
SMTanimur <smtanimurrahman404@gmail.com>
2024-04-14 19:24:52 +0600

    Add ToastContainer component for displaying notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.scss'
 import "../styles/text.scss";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-
+  <ToastContainer/>
 <Component {...pageProps} />
 
     </>
